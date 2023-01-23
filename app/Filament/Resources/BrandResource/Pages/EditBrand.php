55e08dc40d84183ed83ca4463cd8e62a6cdf60de
aurko-appnap <mahmudur@appnap.io>
2023-01-23 12:14:23 +0600

    and brand won't delete if it's not empty

--- app/Filament/Resources/BrandResource/Pages/EditBrand.php
+++ app/Filament/Resources/BrandResource/Pages/EditBrand.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\BrandResource\Pages;
 
-use App\Filament\Resources\BrandResource;
+use App\Models\Product;
 use Filament\Pages\Actions;
+use Filament\Notifications\Notification;
+use Filament\Pages\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\BrandResource;
+use App\Models\Brand;
+use Filament\Notifications\Actions\Action;
 
 class EditBrand extends EditRecord
 {
@@ -13,7 +18,23 @@ class EditBrand extends EditRecord
     protected function getActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make()
+                ->before(function (Brand $record, DeleteAction $action)
+                {
+                    // dd($record);
+                    $temp = Product::where('brand_id' , $record->id)->count();
+                    if($temp != 0)
+                        {
+                            Notification::make()
+                                ->warning()
+                                ->title('Sorry!')
+                                ->body('This brand isn\'t empty!')
+                                ->send();
+                
+                            $action->cancel();
+                        }
+                }),
+
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
         ];
--- app/Filament/Resources/CategoryResource/Pages/EditCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/EditCategory.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\CategoryResource\Pages;
 
-use App\Filament\Resources\CategoryResource;
+use App\Models\Product;
+use App\Models\Category;
 use Filament\Pages\Actions;
+use Filament\Notifications\Notification;
+use Filament\Pages\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\CategoryResource;
 
 class EditCategory extends EditRecord
 {
@@ -13,7 +17,22 @@ class EditCategory extends EditRecord
     protected function getActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make()
+                ->before(function (Category $record, DeleteAction $action)
+                {
+                    // dd($record);
+                    $temp = Product::where('category_id' , $record->id)->count();
+                    if($temp != 0)
+                        {
+                            Notification::make()
+                                ->warning()
+                                ->title('Sorry!')
+                                ->body('This category isn\'t empty!')
+                                ->send();
+                
+                            $action->cancel();
+                        }
+                }),
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
         ];
