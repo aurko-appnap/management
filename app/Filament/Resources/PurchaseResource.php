cc9431e4c79d1acd4ef85d80511fc78445e443c4
aurko-appnap <mahmudur@appnap.io>
2023-02-02 11:22:57 +0600

    Purchase resources, model and migration created.

---
+++ app/Filament/Resources/PurchaseResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PurchaseResource\Pages;
+use App\Filament\Resources\PurchaseResource\RelationManagers;
+use App\Models\Purchase;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PurchaseResource extends Resource
+{
+    protected static ?string $model = Purchase::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-beaker';
+    protected static ?string $navigationGroup = 'Finance';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPurchases::route('/'),
+            'create' => Pages\CreatePurchase::route('/create'),
+            'edit' => Pages\EditPurchase::route('/{record}/edit'),
+        ];
+    }    
+}
