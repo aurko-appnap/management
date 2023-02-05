7c1483fb7f793f3639a92f1cadecd9c6108006b0
aurko-appnap <mahmudur@appnap.io>
2023-02-05 12:24:21 +0600

    inventory added in products table

---
+++ database/migrations/2023_02_05_051129_create_purchase_table_again.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->string('purchase_number')->nullable();
+            $table->string('purchase_status')->nullable();
+            $table->string('supplier_id')->nullable();
+            $table->string('company_id')->nullable();
+            $table->string('total_purchased_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_table_again');
+    }
+};
