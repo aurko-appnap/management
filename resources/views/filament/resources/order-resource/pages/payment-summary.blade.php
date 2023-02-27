3508a3722cbdf86b3e809973737928ef8a4f7fa6
aurko-appnap <mahmudur@appnap.io>
2023-02-27 12:43:53 +0600

    css customised and payment summary on order and purchase complete

--- resources/views/filament/resources/order-resource/pages/payment-summary.blade.php
+++ resources/views/filament/resources/order-resource/pages/payment-summary.blade.php
@@ -1,27 +1,32 @@
 <x-filament::page>
+<link rel="stylesheet" href="{{asset('css/pagination.css')}}">
 <link rel="stylesheet" href="{{asset('css/custom.css')}}">
 
-<table>
-    <caption>Payment Made for this Order</caption>
-    <thead>
-        <th>Serial</th>
-        <th>Payment Amount</th>
-        <th>Payment Method</th>
-        <th>Payment Time</th>
-        <th>Payment Message</th>
-    </thead>
-    <tbody>
-        @foreach ($paymentSummary as $key => $payment)
-        <tr>
-            <td>{{$key+1}}</td>
-            <td>৳ {{number_format($payment->transaction_amount , 2)}}</td>
-            <td>{{$payment->transaction_method}}</td>
-            <td>{{date("d/m/Y h:i:s A", strtotime($payment->created_at))}}</td>
-            <td>{{$payment->transaction_message}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="border border-gray-300 shadow-sm bg-white rounded-xl filament-tables-container dark:bg-gray-800 dark:border-gray-700">
+    <div class="filament-tables-table-container overflow-x-auto relative dark:border-gray-700 rounded-t-xl">
+        <table class="filament-tables-table w-full text-start divide-y table-auto dark:divide-gray-700">
+            <caption class="py-4">Payment Made for this Order</caption>
+            <thead class="bg-gray-500/5">
+                <td class="filament-tables-checkbox-cell w-4 px-4">Serial</td>
+                <td class="filament-tables-header-cell p-2">Payment Amount</td>
+                <td class="filament-tables-header-cell p-2">Payment Method</td>
+                <td class="filament-tables-header-cell p-2">Payment Time</td>
+                <td class="filament-tables-header-cell p-2">Payment Message</td>
+            </thead>
+            <tbody class="divide-y whitespace-nowrap dark:divide-gray-700">
+                @foreach ($paymentSummary as $key => $payment)
+                <tr class="filament-tables-row transition hover:bg-gray-50 dark:hover:bg-gray-500/10">
+                    <td class="filament-tables-cell dark:text-white filament-table-cell-id px-6 py-4">{{$key+1}}</td>
+                    <td class="filament-tables-cell dark:text-white">৳ {{number_format($payment->transaction_amount , 2)}}</td>
+                    <td class="filament-tables-cell dark:text-white px-6">{{$payment->transaction_method}}</td>
+                    <td class="filament-tables-cell dark:text-white">{{date("d/m/Y h:i:s A", strtotime($payment->created_at))}}</td>
+                    <td class="filament-tables-cell dark:text-white px-6">{{$payment->transaction_message}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <?php
 if(count($paymentSummary)>0) $noDataDisplay1 = 'none';
@@ -31,27 +36,31 @@ else $noDataDisplay1 = '';
 
 <br><br>
 <!-- --------------------------------------------------------------- -->
-<table>
-    <caption>Refund Made on this Order</caption>
-    <thead>
-        <th>Serial</th>
-        <th>Payment Amount</th>
-        <th>Payment Method</th>
-        <th>Payment Time</th>
-        <th>Payment Message</th>
-    </thead>
-    <tbody>
-        @foreach ($refundSummary as $key => $payment)
-        <tr>
-            <td>{{$key+1}}</td>
-            <td>৳ {{number_format($payment->transaction_amount , 2)}}</td>
-            <td>{{$payment->transaction_method}}</td>
-            <td>{{date("d/m/Y h:i:s A", strtotime($payment->created_at))}}</td>
-            <td>{{$payment->transaction_message}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="border border-gray-300 shadow-sm bg-white rounded-xl filament-tables-container dark:bg-gray-800 dark:border-gray-700">
+    <div class="filament-tables-table-container overflow-x-auto relative dark:border-gray-700 rounded-t-xl">
+        <table class="filament-tables-table w-full text-start divide-y table-auto dark:divide-gray-700">
+            <caption class="py-4">Refund Made on this Order</caption>
+            <thead class="bg-gray-500/5">
+                <td class="filament-tables-checkbox-cell w-4 px-4">Serial</td>
+                <td class="filament-tables-header-cell p-2">Payment Amount</td>
+                <td class="filament-tables-header-cell p-2">Payment Method</td>
+                <td class="filament-tables-header-cell p-2">Payment Time</td>
+                <td class="filament-tables-header-cell p-2">Payment Message</td>
+            </thead>
+            <tbody class="divide-y whitespace-nowrap dark:divide-gray-700">
+                @foreach ($refundSummary as $key => $payment)
+                <tr class="filament-tables-row transition hover:bg-gray-50 dark:hover:bg-gray-500/10">
+                    <td class="filament-tables-cell dark:text-white filament-table-cell-id px-6 py-4">{{$key+1}}</td>
+                    <td class="filament-tables-cell dark:text-white">৳ {{number_format($payment->transaction_amount , 2)}}</td>
+                    <td class="filament-tables-cell dark:text-white px-6">{{$payment->transaction_method}}</td>
+                    <td class="filament-tables-cell dark:text-white">{{date("d/m/Y h:i:s A", strtotime($payment->created_at))}}</td>
+                    <td class="filament-tables-cell dark:text-white px-6">{{$payment->transaction_message}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 
 <?php
 if(count($refundSummary)>0) $noDataDisplay2 = 'none';
