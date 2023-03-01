9e696bf461f2f33e8f522818203212629fdef0b4
aurko-appnap <mahmudur@appnap.io>
2023-03-01 12:30:10 +0600

    reset result button in brand order summary

--- resources/views/filament/resources/brand-resource/pages/order-summary.blade.php
+++ resources/views/filament/resources/brand-resource/pages/order-summary.blade.php
@@ -24,6 +24,9 @@
     </div>
 </div>
 
+<div class="filament-page-actions flex flex-wrap items-center gap-4 justify-start shrink-0">
+    <a href="{{url('/admin/categories/order-detail/'.$brandID)}}" class="filament-button filament-button-size-md inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">Reset Result</a>
+</div>
 
 <div class="border border-gray-300 shadow-sm bg-white rounded-xl filament-tables-container dark:bg-gray-800 dark:border-gray-700">
     <div class="filament-tables-table-container overflow-x-auto relative dark:border-gray-700 rounded-t-xl">
