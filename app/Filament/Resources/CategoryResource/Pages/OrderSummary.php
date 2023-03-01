f077e58052b90688f7385981c3b4003905f2ef4f
aurko-appnap <mahmudur@appnap.io>
2023-03-01 12:28:50 +0600

    reset result button added

--- app/Filament/Resources/CategoryResource/Pages/OrderSummary.php
+++ app/Filament/Resources/CategoryResource/Pages/OrderSummary.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\CategoryResource\Pages;
 
 use Filament\Resources\Table;
+use Illuminate\Support\Carbon;
 use Filament\Resources\Pages\Page;
 use Illuminate\Support\Facades\DB;
 use App\Filament\Resources\CategoryResource;
@@ -12,15 +13,31 @@ class OrderSummary extends Page
     protected static string $resource = CategoryResource::class;
     public $OrderSummary;
     public $catID;
+    public $totalOrderCount;
 
     public $totalPageCount;
     public $page;
 
+    public $fromDate, $toDate;
+    public $numberOfProduct = 0;
+
     public function mount()
     {
+        if(request('from') != NULL)
+        {
+            $this->fromDate = Carbon::createFromFormat('d/m/Y', request('from')); 
+            $this->toDate = Carbon::createFromFormat('d/m/Y', request('to')); 
+            $perPageRecord = 500000;
+        }
+        else
+        {
+            $this->fromDate = Carbon::createFromFormat('d/m/Y', "01/01/1990"); 
+            $this->toDate = Carbon::now();
+            $perPageRecord = 5;
+        }
+
         $this->catID = request('record');
         
-        $perPageRecord = 5;
         request('p') == null ? $this->page = 1 : $this->page = request('p'); 
         $pageRecordStart = ($this->page-1)*$perPageRecord;
         $OrderSummaryRecord = DB::table('orders')
@@ -41,10 +58,29 @@ class OrderSummary extends Page
             ->join('products' , 'products.id' , '=' , 'order_items.product_id')
             ->join('categories' , 'categories.id' , '=' , 'products.category_id')
             ->select('orders.*' , 'customers.name')
+            ->whereDate('orders.order_placed_on' , '>=' , $this->fromDate)
+            ->whereDate('orders.order_placed_on' , '<=' , $this->toDate)
             ->where('categories.id' , $this->catID)
             ->limit($perPageRecord)
             ->offset($pageRecordStart)
             ->get();
+            // dd($this->OrderSummary);
+            $individualProductCount = DB::table('order_items')
+                ->join('products' , 'products.id' , '=' , 'order_items.product_id')
+                ->join('orders' , 'orders.id' , '=' , 'order_items.order_id')
+                ->select('order_items.product_quantity')
+                ->where('products.category_id' , $this->catID)
+                ->whereDate('orders.order_placed_on' , '>=' , $this->fromDate)
+                ->whereDate('orders.order_placed_on' , '<=' , $this->toDate)
+                ->get();
+            foreach($individualProductCount as $key => $pCount)
+                $this->numberOfProduct = $this->numberOfProduct + $pCount->product_quantity;
+    
+            if(request('from') != NULL)
+                $this->totalOrderCount = sizeof($this->OrderSummary);
+            else    
+                $this->totalOrderCount = sizeof($OrderSummaryRecord);
+    
     }
 
     protected static string $view = 'filament.resources.category-resource.pages.order-summary';
--- resources/views/filament/resources/category-resource/pages/order-summary.blade.php
+++ resources/views/filament/resources/category-resource/pages/order-summary.blade.php
@@ -2,6 +2,32 @@
     <link rel="stylesheet" href="{{asset('css/pagination.css')}}">
     <link rel="stylesheet" href="{{asset('css/custom.css')}}">
 
+    <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/gh/alumuko/vanilla-datetimerange-picker@latest/dist/vanilla-datetimerange-picker.css">
+    <script src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alumuko/vanilla-datetimerange-picker@latest/dist/vanilla-datetimerange-picker.js"></script>    
+
+    <div class="filament-stats grid gap-4 lg:gap-8 md:grid-cols-3">
+    <input type="text" class="p-2 rounded-2xl bg-white shadow dark:bg-gray-800" id="datetimerange-input1" size="20" style="text-align:center">
+    <div class="filament-stats-card relative p-6 rounded-2xl bg-white shadow dark:bg-gray-800 filament-stats-overview-widget-card">
+        <div class="space-y-2">
+            <div class="custom-widget-text">
+                Total Order Count: <span class="custom-widget-count">{{$totalOrderCount}}</span>
+            </div>
+        </div>
+    </div>
+    <div class="filament-stats-card relative p-6 rounded-2xl bg-white shadow dark:bg-gray-800 filament-stats-overview-widget-card">
+        <div class="space-y-2">
+            <div class="custom-widget-text">
+                Total Product Count: <span class="custom-widget-count">{{$numberOfProduct}}</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="filament-page-actions flex flex-wrap items-center gap-4 justify-start shrink-0">
+    <a href="{{url('/admin/categories/order-detail/'.$catID)}}" class="filament-button filament-button-size-md inline-flex items-center justify-center py-1 gap-1 font-medium rounded-lg border transition-colors focus:outline-none focus:ring-offset-2 focus:ring-2 focus:ring-inset dark:focus:ring-offset-0 min-h-[2.25rem] px-4 text-sm text-white shadow focus:ring-white border-transparent bg-primary-600 hover:bg-primary-500 focus:bg-primary-700 focus:ring-offset-primary-700 filament-page-button-action">Reset Result</a>
+</div>
+
 <div class="border border-gray-300 shadow-sm bg-white rounded-xl filament-tables-container dark:bg-gray-800 dark:border-gray-700">
     <div class="filament-tables-table-container overflow-x-auto relative dark:border-gray-700 rounded-t-xl">
         <table class="filament-tables-table w-full text-start divide-y table-auto dark:divide-gray-700">
@@ -27,7 +53,7 @@
                     <td class="filament-tables-cell dark:text-white">{{$order->shipping_address}}</td>
                     <td class="filament-tables-cell dark:text-white">{{$order->shipping_method}}</td>
                     <td class="filament-tables-cell dark:text-white">৳ {{$order->total_price}}</td>
-                    <td class="filament-tables-cell dark:text-white">{{$order->order_status}}</td>
+                    <td class="filament-tables-cell dark:text-white px-6">{{$order->order_status}}</td>
                     <td class="filament-tables-actions-cell px-6 py-4"><a href="{{url('/admin/orders/order-detail/'.$order->id)}}" class="filament-link inline-flex items-center justify-center gap-0.5 font-medium hover:underline focus:outline-none focus:underline text-sm text-primary-600 hover:text-primary-500 dark:text-primary-500 dark:hover:text-primary-400 filament-tables-link-action">View</a></td>
                 </tr>
                 @endforeach
@@ -88,10 +114,36 @@ else $noDataDisplay = '';
 <div class="no_data_text" style="display: {{$noDataDisplay}};">No Data Available!</div>
 
 
-<!-- <div class="pagination">
-    @for($currentPage = 1 ; $currentPage <= $totalPageCount ; $currentPage++)
-    <a class='<?php echo $page==$currentPage ? "active" : "" ?>' href="{{url('/admin/categories/order-detail/'.$catID.'?p='.$currentPage)}}">{{$currentPage}}</a>
-    @endfor
-</div> -->
-
+<script>
+    window.addEventListener("load", function (event) {
+        var startDate, endDate;
+        new DateRangePicker('datetimerange-input1',
+            {
+                showWeekNumbers : true,
+                timePicker24Hour : false,
+                timePicker: true,
+                opens: 'down',
+                ranges: {
+                    'Today': [moment().startOf('day'), moment().endOf('day')],
+                    'Yesterday': [moment().subtract(1, 'days').startOf('day'), moment().subtract(1, 'days').endOf('day')],
+                    'Last 7 Days': [moment().subtract(6, 'days').startOf('day'), moment().endOf('day')],
+                    'This Month': [moment().startOf('month').startOf('day'), moment().endOf('month').endOf('day')],
+                },
+                locale: {
+                    format: "DD/MM/YYYY HH:mm:ss",
+                    firstDay: 6,
+                    monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                }
+            },
+            function (start, end) {
+                // alert("<?php echo 'ok'; ?>");
+                startDate = start.format("DD/MM/YYYY");
+                endDate = end.format("DD/MM/YYYY");
+                var url = "<?php echo url('/admin/categories/order-detail/'.$catID.'?from=') ?>"+startDate+"&to="+endDate;
+                window.location.href = url;
+                // alert(url);
+                // alert(start.format() + " - " + end.format());
+            })
+    });
+</script>
 </x-filament::page>
